6dbc5acc4cd2fd239f9b20dceb43e10fba467d97
wesbin <leegosoo11@naver.com>
2022-09-13 16:36:53 +0900

    first commit

---
+++ back/src/main/kotlin/com/holy/back/BackApplication.kt
@@ -0,0 +1,11 @@
+package com.holy.back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackApplication>(*args)
+}

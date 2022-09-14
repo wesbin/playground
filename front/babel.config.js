d969dd3a1e4886b64980bcdd3987b31500c01202
wesbin <leegosoo11@naver.com>
2022-09-14 20:05:03 +0900

    프론트 글꼴 업로드

--- front/babel.config.js
+++ front/babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  devServer: {
-    overlay: false,
-  },
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
---
+++ front/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  devServer: {
+    overlay: false,
+  },
+  // css: {
+  //   loaderOptions: {
+  //     sass: {
+  //       prependData: `
+  //         @import "@/assets/css/_colors.scss";
+  //         @import "@/assets/css/_field.scss";
+  //         @import "@/assets/css/_common.scss";
+  //       `,
+  //     },
+  //   },
+  // },
+};

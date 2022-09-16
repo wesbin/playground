a783ce16f9a8118362e8fa2d046fda4e797df800
wesbin <leegosoo11@naver.com>
2022-09-16 10:23:22 +0900

    일단 작업 중지 김건우의 그림 필요!

--- front/src/App.vue
+++ front/src/App.vue
@@ -7,23 +7,25 @@
       GUNU
     </div>
     <div class="menu-container">
-      HEADER
+      <div class="menu">만화</div>
+      <div class="menu">소설</div>
+      <div class="menu">ETC</div>
     </div>
     <div class="empty-container"></div>
   </header>
   <div class="content">
     <main class="main">
       <section class="article-section">
-        <article class="article">A</article>
+        <article class="article">검색영역</article>
       </section>
       <section class="article-section">
-        <article class="article">B</article>
-        <article class="article">B</article>
+        <article class="article">랜덤</article>
+        <article class="article">등등</article>
       </section>
       <section class="article-section">
-        <article class="article">C</article>
-        <article class="article">C</article>
-        <article class="article">C</article>
+        <article class="article">원하는거</article>
+        <article class="article">게시판</article>
+        <article class="article">형식으로</article>
       </section>
     </main>
   </div>
